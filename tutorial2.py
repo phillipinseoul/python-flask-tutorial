66c934fd6fcec95d403974691c0f80e8644eafd2
phillip <phillip9807@gmail.com>
2020-09-13 00:37:35 +0900

    add 'name' parameter

--- tutorial2.py
+++ tutorial2.py
@@ -4,9 +4,9 @@ from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return render_template("index.html")
+@app.route("/<name>")
+def home(name):
+    return render_template("index.html", content=name)
 
 
 if __name__ == "__main__":
