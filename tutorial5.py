39de6eed7d6fe5f6fe4e58450777a0e526071b23
phillip <phillip9807@gmail.com>
2020-09-13 01:52:00 +0900

    if already stored in session, retreve it

--- tutorial5.py
+++ tutorial5.py
@@ -13,13 +13,15 @@ def login():
     if request.method == "POST":
         user = request.form["nm"]
         session["user"] = user
-        return redirect(url_for("user", usr=user))
+        return redirect(url_for("user"))
     else:
         return render_template("login.html")
 
 @app.route("/user")
 def user():
-    return f"<h1></h1>"
+    if "user" in session:
+        user = session["user"]
+        return f"<h1>{user}</h1>"
 
 if __name__ == "__main__":
     app.run(debug=True)
