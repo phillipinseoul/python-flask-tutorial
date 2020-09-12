74e5f4326e18976f7c0b247ed36b33757518ce23
phillip <phillip9807@gmail.com>
2020-09-13 01:59:22 +0900

    add logout

--- tutorial5.py
+++ tutorial5.py
@@ -26,5 +26,10 @@ def user():
     else:
         return redirect(url_for("login"))
 
+@app.route("/logout")
+def logout():
+    session.pop("user", None)
+    return redirect(url_for("login"))
+
 if __name__ == "__main__":
     app.run(debug=True)
