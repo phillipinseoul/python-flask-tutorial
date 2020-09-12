7e449f5d9085cd08624f436af618396f7a198956
phillip <phillip9807@gmail.com>
2020-09-13 00:38:25 +0900

    add another param - r

--- tutorial2.py
+++ tutorial2.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/<name>")
 def home(name):
-    return render_template("index.html", content=name)
+    return render_template("index.html", content=name, r=2)
 
 
 if __name__ == "__main__":
