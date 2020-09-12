69c69de280164b3e1c8e2fc967b9941251c16254
phillip <phillip9807@gmail.com>
2020-09-13 00:34:44 +0900

    import render_template - load html templates

--- tutorial2.py
+++ tutorial2.py
@@ -1,20 +1,13 @@
 ### Tutorial 2: HTML Templates ###
 
-from flask import Flask, redirect, url_for
+from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello! This is the main page <h1>Hello</h1>"
+    return render_template("index.html")
 
-@app.route("/<name>")
-def user(name):
-    return f"Hello {name}!"
-
-@app.route("/admin/")
-def admin():
-    return redirect(url_for("user", name="Admin!"))
 
 if __name__ == "__main__":
     app.run()
