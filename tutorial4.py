b1b2736c0d83e90b4143c401fd010913d5958d26
phillip <phillip9807@gmail.com>
2020-09-13 01:28:41 +0900

    create login page

--- tutorial4.py
+++ tutorial4.py
@@ -8,5 +8,13 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    return render_template()
+
+@app.route("/<usr>")
+def user(usr):
+    return f"<h1>{usr}</h1>"
+
 if __name__ == "__main__":
     app.run(debug=True)
