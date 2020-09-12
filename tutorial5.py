6f103ba022161f825f1d0cb760a9b477918e1c0c
phillip <phillip9807@gmail.com>
2020-09-13 01:50:36 +0900

    session - stores user data as dictionary

--- tutorial5.py
+++ tutorial5.py
@@ -1,6 +1,6 @@
 ### Sessions ###
 
-from flask import Flask, redirect, url_for, render_template, request
+from flask import Flask, redirect, url_for, render_template, request, session
 
 app = Flask(__name__)
 
@@ -12,13 +12,14 @@ def home():
 def login():
     if request.method == "POST":
         user = request.form["nm"]
+        session["user"] = user
         return redirect(url_for("user", usr=user))
     else:
         return render_template("login.html")
 
-@app.route("/<usr>")
-def user(usr):
-    return f"<h1>{usr}</h1>"
+@app.route("/user")
+def user():
+    return f"<h1></h1>"
 
 if __name__ == "__main__":
     app.run(debug=True)
