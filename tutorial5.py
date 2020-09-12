27b25ac82e06fa8237a66b6c35b5861976902881
phillip <phillip9807@gmail.com>
2020-09-13 02:03:34 +0900

    set max time for session

--- tutorial5.py
+++ tutorial5.py
@@ -1,9 +1,11 @@
 ### Sessions ###
 
 from flask import Flask, redirect, url_for, render_template, request, session
+from datetime import timedelta
 
 app = Flask(__name__)
 app.secret_key = "hello"
+app.permanent_session_lifetime = timedelta(minutes=5)
 
 @app.route("/")
 def home():
